b72d7f18e9741d4722cc522c1eb3badae224c204
NPN-PC <phuongnam.nguyen241196@gmail.com>
2023-11-02 23:36:32 +0700

    add images and audios

---
+++ src/assets/images/index.ts
@@ -0,0 +1,27 @@
+import hangman from './hangman_title.png';
+import hangman_left from './hangman_left.gif';
+import hangman_right from './hangman_right.gif';
+import animals from './animals.png';
+import sports from './sports.png';
+import body from './body.png';
+import jobs from './jobs.png';
+import countries from './countries.png';
+import home from './home.png';
+import fruits from './fruits.png';
+import transport from './transport.png';
+import speaker from './speaker.png';
+
+export {
+  hangman,
+  hangman_left,
+  hangman_right,
+  animals,
+  sports,
+  body,
+  jobs,
+  countries,
+  home,
+  fruits,
+  transport,
+  speaker
+};
